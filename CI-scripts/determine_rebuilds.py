6ff88823f8f2d4d0ad1efdfdb04c9d1bdea57314
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-09-01 02:05:41 -0400

    github workflow documentation is so damn bad

--- CI-scripts/determine_rebuilds.py
+++ CI-scripts/determine_rebuilds.py
@@ -7,6 +7,7 @@ that the builds happen in the correct order.
 
 
 import sys
+from os import getenv
 from pathlib import Path
 
 from image_tree import ImageTree
@@ -31,12 +32,15 @@ def determine_rebuilds(tree, edited_names):
 
 
 def main():
-    repo_path = Path(__file__).resolve().parents[1]
+    repo_path = Path(getenv("GITHUB_WORKSPACE"))
+    data_dir = Path(getenv("DATA_DIR"))
     diff_tree_paths = sys.argv[1]
     updated_images = [path.replace('/Dockerfile', '')
                       for path in diff_tree_paths.splitlines()]
     image_tree = ImageTree(repo_path)
     to_rebuild = determine_rebuilds(image_tree, updated_images)
+    to_rebuild_lines = '\n'.join(to_rebuild)
+    data_dir.joinpath('to_rebuild.txt').write_text(to_rebuild_lines)
     return to_rebuild
 
 
