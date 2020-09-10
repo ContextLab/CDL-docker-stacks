96998e10ac7b253a20db17c21a05815d2a50d514
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-09-10 00:23:59 -0400

    tests for cdl-base

--- cdl-base/ci/tests/test_cdl_base.py
+++ cdl-base/ci/tests/test_cdl_base.py
@@ -1,5 +1,10 @@
-import pytest
-
-
 def test_utf8_locale(container):
-    
+    c = container.run('echo $LANG')
+    output = c.logs().decode('utf-8').strip()
+    assert output == 'C.UTF-8'
+
+
+def test_apt_cache_removed(container):
+    c = container.run('ls --almost-all /var/lib/apt/lists')
+    output = c.logs().decode('utf-8').strip()
+    assert len(output) == 0
