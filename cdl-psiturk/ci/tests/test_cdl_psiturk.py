6a3f09579d16fd3c4f0b8df5d1fc72a522a257c5
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-09-15 16:57:52 -0400

    added some tests... need to figure out how to test server without access keys

---
+++ cdl-psiturk/ci/tests/test_cdl_psiturk.py
@@ -0,0 +1,27 @@
+import pytest
+
+
+# TODO: figure out a way to make this possible. Requires both PsiTurk
+#  and AWS access keys be present in .psiturkconfig, but no way to
+#  provide secure access to GitHub secrets in pull request workflows
+# def test_psiturk_server():
+#     pass
+
+
+def test_psiturk_default_cmd(container):
+    c = container.run(command=None, shell=None)
+    default_cmd = c.attrs.get('Config').get('Cmd')
+    assert default_cmd == ['psiturk']
+
+
+@pytest.mark.custom_build_test
+def test_mturk_options(container, conda_env):
+    """if MTURK=true is passed as build-arg, should have pymysql installed"""
+    container.expected_attrs.pop('apt_packages')
+    installed_pymysql = conda_env.installed_packages.get('pymysql')
+    assert (installed_pymysql is not None,
+            '--build-arg MTURK=true was passed but PyMySQL is not installed')
+    requested_pymysql = conda_env.requested_packages.get('pymysql')
+    assert (installed_pymysql.matches_version(requested_pymysql),
+            f'installed PyMySQL version ({installed_pymysql.version}) does '
+            f'not match requested version ({requested_pymysql.version})')
