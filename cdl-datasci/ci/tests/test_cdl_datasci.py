af685d13d147950df88f3173b4aaf8e2564dbf44
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-09-18 00:46:40 -0400

    pytorch tests and other small updates

--- cdl-datasci/ci/tests/test_cdl_datasci.py
+++ cdl-datasci/ci/tests/test_cdl_datasci.py
@@ -1,3 +1,4 @@
 """
 currently, no additional tests are needed on top of parent image tests
 """
+# TODO: write tests for this
---
+++ cdl-pytorch/ci/tests/test_cdl_pytorch.py
@@ -0,0 +1,22 @@
+def test_no_cuda_env_set(container):
+    env = container.run('printenv', detach=False, remove=True, tty=False)
+    assert 'NO_CUDA=1' in env
+
+
+def test_pytorch(container):
+    cmd = """"
+    import torch
+    torch.manual_seed(0)
+    x = torch.rand(5).tolist()
+    for i in x:
+        print(round(i, 8))
+    """.replace('\n    ', '\n')
+    expected = ['0.49625659', '0.7682218', '0.08847743', '0.13203049', '0.30742282']
+    output = container.run(command=cmd, shell='python', detach=False, remove=True)
+    result = output.splitlines()
+    assert result == expected
+
+
+
+
+
