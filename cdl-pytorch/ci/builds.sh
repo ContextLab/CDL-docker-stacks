a980978b4b813a9ffc89dd1063fb961de3fe4aea
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-09-20 20:48:40 -0400

    added build scripts for cdl-pytorch

---
+++ cdl-pytorch/ci/builds.sh
@@ -0,0 +1,28 @@
+# functions for building various versions of image
+
+build_default() {
+    local image_dir="$GITHUB_WORKSPACE/cdl-pytorch"
+    docker build --rm --force-rm \
+        -f "$image_dir/Dockerfile" \
+        -t "$DOCKER_HUB_ORG/cdl-pytorch:$PYTHON_VERSION" \
+        --build-arg PYTHON_VERSION="$PYTHON_VERSION" \
+        "$image_dir"
+}
+
+build_custom() {
+    local image_dir="$GITHUB_WORKSPACE/cdl-pytorch"
+    # passing --build-arg without a value causes environment variable with
+    # matching name to be used
+    source "$GITHUB_WORKSPACE/cdl-pytorch/ci/custom-args.sh"
+    docker build --rm --force-rm \
+        -f "$image_dir/Dockerfile" \
+        -t "$DOCKER_HUB_ORG/cdl-pytorch:${PYTHON_VERSION}-custom" \
+        --build-arg PYTHON_VERSION \
+        --build-arg APT_PACKAGES \
+        --build-arg CONDA_PACKAGES \
+        --build-arg PIP_VERSION \
+        --build-arg PIP_PACKAGES \
+        --build-arg WORKDIR \
+        --build-arg PORT \
+        "$image_dir"
+}
