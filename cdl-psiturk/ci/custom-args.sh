f4eab8cb6c26e5fbc5f636401a7f9fc81582d9f4
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-09-22 02:24:31 -0400

    fixed custom build arg pip version that conflicted with pinned spec

--- cdl-psiturk/ci/custom-args.sh
+++ cdl-psiturk/ci/custom-args.sh
@@ -8,7 +8,7 @@ export APT_PACKAGES="curl htop"
 # extra version-pinned conda packages
 export CONDA_PACKAGES="numpy=1.19.1 requests=2.24.0"
 # older but recent pip version
-export PIP_VERSION="19.3.1"
+export PIP_VERSION="20.0.2"
 # extra pip packages (including a GitHub install)
 export PIP_PACKAGES="quail==0.2.0 git+https://github.com/ContextLab/hypertools.git@dea0df127b150c39ed9a7b1faaa8fbde089ee834"
 # alternate working directory
