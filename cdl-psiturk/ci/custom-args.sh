f00d05bd5d07d2e2d924d60f076d517188317084
paxtonfitzpatrick <paxton.c.fitzpatrick.19@dartmouth.edu>
2020-09-27 22:15:58 -0400

    bump hypertools commit hash two other places it was missed

--- cdl-psiturk/ci/custom-args.sh
+++ cdl-psiturk/ci/custom-args.sh
@@ -10,7 +10,7 @@ export CONDA_PACKAGES="numpy=1.19.1 requests=2.24.0"
 # older but recent pip version
 export PIP_VERSION="20.0.2"
 # extra pip packages (including a GitHub install)
-export PIP_PACKAGES="quail==0.2.0 git+https://github.com/ContextLab/hypertools.git@dea0df127b150c39ed9a7b1faaa8fbde089ee834"
+export PIP_PACKAGES="quail==0.2.0 git+https://github.com/ContextLab/hypertools.git@3f45375682a8f12a1278dd1720290d75a50062a9"
 # alternate working directory
 export WORKDIR="/myworkdir"
 # build for use with MTurk
